06ef8e90c63e03f42a57c15253f0967880aa0856
Daniel <mahdakdaniel@gmail.com>
2023-05-31 10:28:18 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(pocet_kol)
     basic.clearScreen()
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onButtonPressed(Button.B, function () {
+    basic.showString("START")
     for (let index = 0; index < pocet_kol; index++) {
-        basic.showString("START")
         start = false
         basic.pause(randint(1000, 5000))
         start = true
@@ -20,10 +20,12 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
             if (input.pinIsPressed(TouchPin.P1)) {
                 basic.showString("P1")
                 start = false
+                P1 += 1
             } else {
                 if (input.pinIsPressed(TouchPin.P2)) {
                     basic.showString("P2")
                     start = false
+                    P2 += 1
                 }
             }
         }
@@ -37,6 +39,17 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (P1 < P2) {
         basic.showString("P2 won!")
     }
+    if (P1 == P2) {
+        basic.showString("Tie!")
+    }
+    pocet_kol = 0
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 })
 let start = false
 let pocet_kol = 0
